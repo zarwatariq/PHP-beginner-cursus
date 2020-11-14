2e3e8cd758ca354b42184fbd0532f6b7511bd704
Zarwa <ztariq@roc-dev.com>
2020-11-14 15:02:21 +0100

    opdracht 2 van hoofdstuk 1 klaar

---
+++ 002/opdracht-1.php
@@ -0,0 +1,9 @@
+<?php
+$a = "Ik heb niet gefaald";
+$b = "die niet werken";
+$c = 10000;
+
+echo "{$a}. ik heb allen {$c} manieren gevonden {$b}. <br>";
+echo "-Thomas Edison <br>";
+
+?>
---
+++ 002/opdracht-2.php
@@ -0,0 +1,9 @@
+<?php
+$a = "die geen fout";
+$b = "en";
+$c = "niet";
+
+echo " M{$b}s{$b} {$a}{$b}, werk{$b} {$c} <br>";
+echo "-Albert Plesman (KLM)<br>";
+
+?>
