2fbb01aacd5af8393fde38f474b1a22b82d0b9dc
Zarwa <ztariq@roc-dev.com>
2020-11-14 15:28:03 +0100

    op 3 and 4

---
+++ 002/opdracht-3.php
@@ -0,0 +1,9 @@
+<?php
+$a = "ry";
+$b = "but";
+$c = "fail";
+
+echo " T{$a} and {$c}, but never {$c} to t{$a}.  <br>";
+echo "-Tyra Banks<br>";
+
+?>
---
+++ 002/opdracht-4.php
@@ -0,0 +1,9 @@
+<?php
+$a = "\"omgaan";
+$b = "met";
+$c = "teleu";
+
+echo " De cursus {$a} {$b} {$c}rstellingen ” kan vanavond helaas niet doorgaan.<br>";
+echo "- Herman Finkers<br>";
+
+?>
