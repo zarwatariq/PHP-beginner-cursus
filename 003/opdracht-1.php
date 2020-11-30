f00fae22b6d2a201bb42800916892faaff84d162
Zarwa <ztariq@roc-dev.com>
2020-11-30 03:15:19 +0100

    chapter 3 op 1 test

--- 003/opdracht-1.php
+++ 003/opdracht-1.php
@@ -1,6 +1,20 @@
-<?php
-for ( $i = 0; $i < 10 ; $i++){
-    echo "image";
-}
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Hoofdstuk-3 op-1</title>
+            <style>
+                img{
+                    display: table-row;
+                }
+            </style>
+        </head>
 
-?>
+        <body>
+            <?php
+                for ( $i = 1; $i <= 10 ; $i++){
+                echo '<img src="apen/aap1 ' .$i. '.jpg">';
+                }
+            ?>
+        </body>
+    </html>
