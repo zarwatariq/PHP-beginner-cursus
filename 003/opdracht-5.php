8a27d8f447216a635af00fe600cd774b062c1911
Zarwa <ztariq@roc-dev.com>
2021-01-12 10:05:17 +0100

    aap file

--- 003/opdracht-5.php
+++ 003/opdracht-5.php
@@ -3,11 +3,32 @@
         <head>
             <meta charset="UTF-8">
             <title>Hoofdstuk-3 op-5</title>
+            <style>
+                body{
+                    text-align: center;
+                }
+                img {
+                    width: 150px;
+                }
+                img.green-border {
+                    border: solid 2px green;
+                }
+                img.red-border {
+                    border: solid 2px red;
+                }
+            </style>
         </head>
-
         <body>
             <?php
-
+                $borderColor = "red";
+                $borderColor = "green";
+               for ($i = 1; $i <= 9 ; $i++) {
+                   if ($i % 2 == 0) {
+                   } else {
+                   }
+                   echo'<img class="' . $borderColor . '-border" src="apen/aap ' . $i . '.jpg"alt="Aap ' . $i . '">';
+               }
             ?>
+
         </body>
     </html>
